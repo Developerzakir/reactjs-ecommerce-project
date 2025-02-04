9eb834d9df626510dc74e53314eab6f29b9db699
zakir Hossen <zakirhossen082@gmail.com>
2025-02-04 09:44:52 +0600

    home page category tab products filtering

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './Banner.jsx';
 import HomeCategory from './HomeCategory.jsx';
+import CategoryShowCase from './CategoryShowCase.jsx';
 
 const Home = () => {
   return (
     <div>
        <Banner />
        <HomeCategory />
+       <CategoryShowCase />
       
     </div>
   )
