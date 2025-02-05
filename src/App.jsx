b80d70886bac44969c7fe4d1bb6c5dedb1962253
zakir Hossen <zakirhossen082@gmail.com>
2025-02-05 12:20:29 +0600

    home page footer design

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <>
       <Navbar />
       <Outlet />
+      <Footer />
     </>
   )
 }
