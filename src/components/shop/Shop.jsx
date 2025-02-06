3bcc018d030585ca963851b8963d6631917199d3
zakir Hossen <zakirhossen082@gmail.com>
2025-02-06 09:36:14 +0600

    shop page right section most popular blog section

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -5,6 +5,7 @@ import ProductCards from './ProductCards'
 import Pagination from './Pagination'
 import Search from './Search'
 import ShopCategory from './ShopCategory'
+import PopularPost from './PopularPost'
 
 const showResults = "Showing 01 - 12 of 140 Results"
 
@@ -82,6 +83,7 @@ const Shop = () => {
                               setProducts={setProducts}
                               selectedCat={selectedCat}
                              />
+                             <PopularPost />
                         </aside>
                     </div>
                 </div>
